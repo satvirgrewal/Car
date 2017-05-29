251853bbeadf15df99b0f5074ce5b387b5d9c3fd
satvirgrewal <satvir.grewal03@gmail.com>
2017-05-28 20:38:28 -0500

    defining Cars table structure

--- database/migrations/2017_05_28_225400_create_cars_table.php
+++ database/migrations/2017_05_28_225400_create_cars_table.php
@@ -15,6 +15,9 @@ class CreateCarsTable extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('make');
+            $table->string('model');
+            $table->date('produced_on');
             $table->timestamps();
         });
     }
